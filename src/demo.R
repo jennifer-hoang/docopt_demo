49bdf1c89509d7fb6ed71961a1ff5a22cef23a2e
Jennifer Hoang <jennifer.anh.hoang@gmail.com>
2021-11-16 21:30:49 -0800

    Add main function in demo.R

--- src/demo.R
+++ src/demo.R
@@ -6,7 +6,7 @@
 "This script prints out docopt args.
 Usage: demo.R <arg1> --arg2=<arg2> [<arg3>] [--arg4=<arg4>]
 Options:
-<arg>             Takes any value (this is a required positional argument)
+<arg1>             Takes any value (this is a required positional argument)
 --arg2=<arg2>     Takes any value (this is a required option)
 [<arg3>]          Takes any value (this is an optional positional argument)
 [--arg4=<arg4>]   Takes any value (this is an optional option)
@@ -14,6 +14,11 @@ Options:
 
 library(docopt)
 opt <- docopt(doc)
-print(opt)
-print(typeof(opt))
-print(opt$arg3)
+
+main <- function(opt){
+  print(opt)
+  print(typeof(opt))
+  print(opt$arg3)  
+}
+
+main(opt)
